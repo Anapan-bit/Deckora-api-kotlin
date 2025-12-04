2c550065cc002e439f2ef2b3bb158061fb0b8f05
Anapan-bit <anaispalmasanchez@gmail.com>
2025-12-04 16:12:39 -0300

    Mas cambios

--- src/main/java/com/deckora/Dekora/controller/UsuarioController.java
+++ src/main/java/com/deckora/Dekora/controller/UsuarioController.java
@@ -1,4 +1,5 @@
 package com.deckora.Dekora.controller;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -27,15 +28,13 @@ import com.deckora.Dekora.service.UsuarioService;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
 @RestController
 @RequestMapping("/api/v2/usuarios")
-//tag es para dar el titulo en el swagger de la usuarios
+// tag es para dar el titulo en el swagger de la usuarios
 @Tag(name = "Usuarios", description = "Operaciones relacionadas con la creacion, modificación y eliminación de las usuarios.")
 public class UsuarioController {
 
@@ -45,58 +44,64 @@ public class UsuarioController {
     @Autowired
     private UsuarioModelAssembler usuarioAssembler;
 
-    //get de todos las usuarios
+    // get de todos las usuarios
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
-    //operation es para el resumen de lo que hace
-    //paramater es para describir el tipo de dato esperado
+    // operation es para el resumen de lo que hace
+    // paramater es para describir el tipo de dato esperado
     @Operation(summary = "Este método obtiene todas las usuarios", description = "Muestra una lista de todas las usuarios creadas")
-    public ResponseEntity<CollectionModel<EntityModel<Usuario>>> getAllUsuarios(){
-        List <EntityModel<Usuario>> listaUsuarios = usuarioService.findAll().stream()
+    public ResponseEntity<CollectionModel<EntityModel<Usuario>>> getAllUsuarios() {
+        List<EntityModel<Usuario>> listaUsuarios = usuarioService.findAll().stream()
                 .map(usuarioAssembler::toModel)
                 .collect(Collectors.toList());
-        if(listaUsuarios.isEmpty()){
+        if (listaUsuarios.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(CollectionModel.of(
-            listaUsuarios,
-            linkTo(methodOn(UsuarioController.class).getAllUsuarios()).withSelfRel()
-        ));
+                listaUsuarios,
+                linkTo(methodOn(UsuarioController.class).getAllUsuarios()).withSelfRel()));
     }
 
-    //get x id
+    // get x id
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     @Operation(summary = "Este método obtiene una usuario en específico", description = "A través de un id, este método muestra una usuario en específico")
-    public ResponseEntity<EntityModel<Usuario>> getUsuarioById(@Parameter(description = "Id del usuario", required = true, example = "1")@PathVariable Long id){
+    public ResponseEntity<EntityModel<Usuario>> getUsuarioById(
+            @Parameter(description = "Id del usuario", required = true, example = "1") @PathVariable Long id) {
         Usuario usuario = usuarioService.findById(id);
-        if (usuario == null){
+        if (usuario == null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(usuarioAssembler.toModel(usuario));
     }
 
-    //post crear un usuario
+    // post crear un usuario
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
     @Operation(summary = "Este método crea una usuario", description = "Crea nueva usuario, enviando un objeto a través de un POST")
-    public ResponseEntity<EntityModel<Usuario>> createUsuario(@Parameter(description = "Detalles del usuario", required = true)@RequestBody Usuario usuario){
+    public ResponseEntity<EntityModel<Usuario>> createUsuario(
+            @Parameter(description = "Detalles del usuario", required = true) @RequestBody Usuario usuario) {
         Usuario newUsuario = usuarioService.save(usuario);
         return ResponseEntity
-                .created(linkTo(methodOn(UsuarioController.class).getUsuarioById(Long.valueOf(newUsuario.getId()))).toUri())
+                .created(linkTo(methodOn(UsuarioController.class).getUsuarioById(Long.valueOf(newUsuario.getId())))
+                        .toUri())
                 .body(usuarioAssembler.toModel(newUsuario));
     }
 
-    //put para usuario
+    // put para usuario
     @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     @Operation(summary = "Este método actualiza una usuario", description = "A través de un id, este método actualiza una usuario, pero se deben escribir todos los atributos")
-    public ResponseEntity<EntityModel<Usuario>> updateUsuario(@Parameter(description = "Id del usuario", required = true, example = "1")@PathVariable Long id, @Parameter(description = "Detalles del usuario", required = true)@RequestBody Usuario usuario){
-        usuario.setId(usuario.getId()); //revisar despues
+    public ResponseEntity<EntityModel<Usuario>> updateUsuario(
+            @Parameter(description = "Id del usuario", required = true, example = "1") @PathVariable Long id,
+            @Parameter(description = "Detalles del usuario", required = true) @RequestBody Usuario usuario) {
+        usuario.setId(usuario.getId()); // revisar despues
         Usuario updateUsuario = usuarioService.save(usuario);
         return ResponseEntity.ok(usuarioAssembler.toModel(updateUsuario));
     }
 
-    //Patch usuario
+    // Patch usuario
     @PatchMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     @Operation(summary = "Este método puede modificar un campo en específico en una usuario", description = "A través de un id, este método actualiza una usuario, este método actualiza solo el atributo que nosotros queremos modificar")
-    public ResponseEntity<EntityModel<Usuario>> patchUsuario(@Parameter(description = "Id del usuario", required = true, example = "1")@PathVariable Long id, @Parameter(description = "Campo a modificar", required = true)@RequestBody Usuario parcialUsuario){
+    public ResponseEntity<EntityModel<Usuario>> patchUsuario(
+            @Parameter(description = "Id del usuario", required = true, example = "1") @PathVariable Long id,
+            @Parameter(description = "Campo a modificar", required = true) @RequestBody Usuario parcialUsuario) {
         Usuario patched = usuarioService.patchUsuario(id, parcialUsuario);
         if (patched == null) {
             return ResponseEntity.notFound().build();
@@ -104,23 +109,23 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioAssembler.toModel(patched));
     }
 
-    @PostMapping(value = "/login", produces = MediaTypes.HAL_JSON_VALUE)
-    @Operation(summary = "Login de usuario", description = "Verifica las credenciales y devuelve el usuario si son correctas")
-    public ResponseEntity<?> loginUsuario(@Parameter(description = "Credenciales de login", required = true)@RequestBody LoginRequest loginRequest) {
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> loginUsuario(@RequestBody LoginRequest loginRequest) {
+
         Optional<Usuario> usuarioOpcional = usuarioService.findByNombreUsuario(loginRequest.getNombre_usuario());
 
-        if (usuarioOpcional.isPresent()) {
-            Usuario usuario = usuarioOpcional.get();
-            if (usuario.getContrasenia_usuario().equals(loginRequest.getContrasenia_usuario())) {
-                // Devuelve el usuario como EntityModel
-                return ResponseEntity.ok(usuarioAssembler.toModel(usuario));
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                                    .body("Contraseña incorrecta");
-            }
-        } else {
+        if (usuarioOpcional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                .body("Usuario no encontrado");
+                    .body("Usuario no encontrado");
         }
+
+        Usuario usuario = usuarioOpcional.get();
+
+        if (!usuario.getContrasenia_usuario().equals(loginRequest.getContrasenia_usuario())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Contraseña incorrecta");
+        }
+
+        return ResponseEntity.ok(usuario);
     }
 }
